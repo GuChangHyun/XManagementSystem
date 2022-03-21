b74d4525a1d272e781966673cf7e34cd600faf29
GuChangHyun <hyunguin9@gmail.com>
2022-03-21 23:22:42 +0900

    add menu and change subjects

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,12 +1,54 @@
+import java.util.Scanner;
 
-public class MenuManager {
-
-	public static void main(String[] args) {
-		System.out.println("1. Add Students");
-		System.out.println("2. Delete Students");
-		System.out.println("3. Edit Students");
-		System.out.println("4. View Students");
-		System.out.println("5. Exit");
+public class MenuManager
+{
+	public static void main(String[] args)
+	{
+		int num = 0;
+		Scanner input = new Scanner(System.in);
+		
+		while(num != 6)
+		{
+			System.out.println("*** Student Management System Menu ***");
+			System.out.println("1. Add Products");
+			System.out.println("2. Delete Products");
+			System.out.println("3. Edit Products");
+			System.out.println("4. View Products");
+			System.out.println("5. Show a menu");
+			System.out.println("6. Exit");
+			System.out.println("Select one number between 1~6:");
+			num = input.nextInt();
+			if (num == 1)
+			{
+				System.out.print("ProductId:");
+				int ProductId = input.nextInt();
+				System.out.print("Product Name:");
+				String ProductName = input.next();
+			}
+			else if (num == 2)
+			{
+				System.out.println("2");
+			}
+			else if (num == 3)
+			{
+				System.out.println("3");
+			}
+			else if (num == 4)
+			{
+				System.out.println("4");
+			}
+			else if (num == 5)
+			{
+				System.out.println("5");
+			}
+			else if (num == 6)
+			{
+				System.out.println("6");
+			}
+			else
+			{
+				continue;
+			}
+		}
 	}
-
 }
